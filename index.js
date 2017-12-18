3d20fc9e0c662b9cc9cc3f151540881a9b38b5f4
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-12-18 19:07:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "let's talk about space";
+var topic = "let's talk about space.s";
 
 var conversation = `Let\'s talk about ${topic}`;
